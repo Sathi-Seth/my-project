5f2069c3f2da2f72d6723f465f3ae2342ea6f187
Sathi-Seth <sathiseth2301@gmail.com>
2025-11-22 20:34:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+let inp = document.querySelector("#input");
+let addBtn = document.querySelector("#AddTask");
+let taskList = document.querySelector("#taskList");
+
+addBtn.addEventListener('click' , () => {
+    let task = inp.value.trim() ; // Read the input value
+    
+    if(!task){ //If empty → do nothing
+        return ;
+    }
+
+    let li = document.createElement('li') ; //Create a new <li>
+    li.innerText = task ;
+
+    let finishBtn = document.createElement('button') ;
+    finishBtn.classList = "finishBtn" ;
+    finishBtn.innerText = 'completed' ;
+    li.appendChild(finishBtn) ;
+
+    let delBtn = document.createElement('button') ; //Add text + delete button
+    delBtn.innerText = 'delete' ;
+    delBtn.classList = "delBtn" ;
+    li.appendChild(delBtn) ;
+    
+    taskList.appendChild(li) ;
+    inp.value = "" ;
+
+}) ;
+
+taskList.addEventListener('click' , (el) => {
+    if (el.target.classList.contains("delBtn"))
+        el.target.parentElement.remove() ;
+}) ;
+
+taskList.addEventListener('click' , (el) => {
+    if(el.target.classList.contains("finishBtn")){
+        el.target.classList.toggle("done");
+    }
+}) ;
+
+
